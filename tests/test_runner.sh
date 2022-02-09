e3c987f8afb3b83edcfdd60c63886774906f9e2f
BureevFedor <63223529+BureevFedor@users.noreply.github.com>
2022-02-10 00:36:47 +0300

    Update test_runner.sh

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-tests = `ls *.in`
+tests = "test_sum_1 test_sum_2"
 echo $tests
 
 for test in $tests; do
 	echo $test
+	./calc <tests/$test.in >$test.res
+	cat $test.res
+	diff tests/$test.out $test.res
 done
